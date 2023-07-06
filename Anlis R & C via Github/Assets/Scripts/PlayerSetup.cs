d2f6dee4194dc2d89eeea615c6a2899701bf5764
Nanoriddler <schlatter.c@gmx.ch>
2023-07-06 11:34:04 +0200

    dick

--- Anlis R & C via Github/Assets/Scripts/PlayerSetup.cs
+++ Anlis R & C via Github/Assets/Scripts/PlayerSetup.cs
@@ -4,14 +4,13 @@ using UnityEngine;
 
 public class PlayerSetup : MonoBehaviour
 {
-    public SFPSC_PlayerMovement movement;
+ 
    
     public GameObject MainCamera;
     public GameObject WeaponCamera;
 
     public void IsLocalPlayer()
     {
-        movement.enabled = true;
 
         MainCamera.SetActive(true);
         WeaponCamera.SetActive(true);
